51af68bc71ef6af889a66b47db792076455bbc0b
NadjaPyvovar <nadja.pyvovar@gmail.com>
2026-08-24 11:38:05 +0200

    correct hw based on review

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -7,6 +7,6 @@ export const connectMongoDB = async () => {
     console.log("✅ MongoDB connection established successfully");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
-    throw error;
+    process.exit(1);
   }
 };
