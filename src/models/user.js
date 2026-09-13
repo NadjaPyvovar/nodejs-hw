57535d65529da5e9f03c78e42f3e380a352ff014
NadjaPyvovar <nadja.pyvovar@gmail.com>
2026-09-13 17:05:30 +0200

    fanalise hw

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// defaulting the username to the email when a new user is created
+userSchema.pre("save", function (next) {
+  if (this.isNew && !this.username) {
+    this.username = this.email;
+  }
+  next();
+});
+
+// stripping the password prior to the document to be serialized to JSON
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const User = model("User", userSchema);
+
+// notes: pre("save") = runs before each save; this.isNew = restricts the default to documents creation only, this will not reset username on later updates/refreshes; toJSON = overriding Mongoose way how the documents get converted to plain objects for res.json(...) (as res.json() calls JSON.stringify, which calls .toJSON() under the hood, guaranteeing the password never leaks in any response)
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
 
+import { authenticate } from "../middleware/authenticate.js";
+
 import {
   getAllNotes,
   getNoteById,
@@ -17,6 +19,8 @@ import {
 } from "../validations/notesValidation.js";
 
 const router = Router();
+
+router.use(authenticate);
 
 // to each route, adding celebrate(schema) middleware to validate request data before reaching the controller
 router.get("/notes", celebrate(getAllNotesSchema), getAllNotes);
